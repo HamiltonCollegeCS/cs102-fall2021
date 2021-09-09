ab332dcffa9cd4d26420ada515ab973094d1f5f2
Darren Strash <dstrash@hamilton.edu>
2021-09-09 13:33:29 -0400

    Tvarps starter code

---
+++ lectures/lecture05/tvarps.cpp
@@ -0,0 +1,13 @@
+/**
+ *  Compute tax from user-entered tvarps
+ *  @file tvarps.cpp
+ *  @author Darren Strash
+ *  @collaborators CS 102, Fall 2021
+ *  @date September 9, 2021
+ */
+#include <iostream>
+
+// Controls operation of program.
+int main() {
+    return 0;
+}
